d069e1e87eb07f3d6ddfb3273d374f958e61b6b6
mahmoudrabbas <mahmoudrabass11@gmail.com>
2025-07-07 19:41:48 +0300

    project setup

---
+++ src/main/java/com/event_ticket/EventTicketApplication.java
@@ -0,0 +1,13 @@
+package com.event_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketApplication.class, args);
+	}
+
+}
